1cbecfb8709f56f97b6db17fcf37bc5204ba81d1
aminbabar <amin.babar1@gmail.com>
2021-07-21 16:45:34 -0400

    working on creating artwork tables

--- art_share_API/db/schema.rb
+++ art_share_API/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_201336) do
+ActiveRecord::Schema.define(version: 2021_07_21_202929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
+    t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
